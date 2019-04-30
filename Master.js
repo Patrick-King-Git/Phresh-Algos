46164cd39eaf9b0d00d206e8b3d464a41701ad27
Patrick <kingpa.gmail.com>
2019-04-30 18:00:28 -0400

    hexadecimal work

---
+++ Master.js
@@ -0,0 +1,10 @@
+/* Number Bases*/
+const {DecToHexaDecimal,
+    NumtoHexaLetters,
+    HexaDecimaltoNum,
+    HexaLetterstoNum,
+    BinToHexa} = require('./NumberBases/hexadecimal');
+const {NumberToDiffBase, NumtoBaseLetters} = require('./NumberBases/n-base');
+const {} = require('./NumberBases/binary');
+/************************************************************/
+
